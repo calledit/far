8a8c760be27bf19ee611f441957ac98a71e74102
calledit <1573053+calledit@users.noreply.github.com>
2024-08-22 13:25:08 +0200

    Update install_far.sh

--- install_far.sh
+++ install_far.sh
@@ -10,14 +10,3 @@ cd mp3d_loftr
 
 wget https://fouheylab.eecs.umich.edu/~cnris/far/model_checkpoints/mp3d_loftr/pretrained_models.zip --no-check-certificate
 unzip pretrained_models.zip
-
-mkdir data
-
-cd data
-
-wget http://example.com/video.mp4
-mkdir imgs_4fps
-
-ffmpeg -i video.mp4 -vf 'scale=640:480' imgs_4fps/%04d.png
-
-scripts/demo.sh
