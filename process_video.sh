1ada5054646bc699637c904f4474fdacf71a184c
calledit <1573053+calledit@users.noreply.github.com>
2024-08-22 13:23:40 +0200

    Create process_video.sh

---
+++ process_video.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source activate far
+
+cd mp3d_loftr
+mkdir data
+cd data
+rm -r imgs_4fps
+wget -O video.mp4 $3
+mkdir imgs_4fps
+
+ffmpeg -i video.mp4 -vf 'scale=640:480' imgs_4fps/%04d.png
+
+cd ..
+cd ..
+scripts/demo.sh $1 $2
