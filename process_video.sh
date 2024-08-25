a093c610b103f2bbb8e05e540cbfa5b14ea6ed41
calledit <1573053+calledit@users.noreply.github.com>
2024-08-25 21:16:08 +0200

    more frames

--- process_video.sh
+++ process_video.sh
@@ -9,7 +9,7 @@ rm -r imgs_4fps
 wget -O video.mp4 $3
 mkdir imgs_4fps
 
-ffmpeg -i video.mp4 -vf 'scale=640:480' imgs_4fps/%04d.png
+ffmpeg -i video.mp4 -vf 'scale=640:480' imgs_4fps/%06d.png
 
 cd ..
 scripts/demo.sh $1 $2
