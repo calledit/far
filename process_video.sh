fdb2c3e2b36dc3f526c343621fc4346feba01f69
calledit <1573053+calledit@users.noreply.github.com>
2024-08-27 18:57:45 +0200

    prepare for masks

--- process_video.sh
+++ process_video.sh
@@ -6,10 +6,14 @@ cd mp3d_loftr
 mkdir data
 cd data
 rm -r imgs_4fps
+rm -r masks_4fps
 wget -O video.mp4 $3
+wget -O mask.mp4 $4
 mkdir imgs_4fps
+mkdir masks_4fps
 
-ffmpeg -i video.mp4 -vf 'scale=640:480' imgs_4fps/%06d.png
+ffmpeg -i video.mp4 -vf 'fps=4,scale=640:480' imgs_4fps/%06d.png
+ffmpeg -i mask.mp4 -vf 'fps=4,scale=640:480' masks_4fps/%06d.png
 
 cd ..
 scripts/demo.sh $1 $2
