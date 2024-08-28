bd4601d68c3fd3126b617102f14c3c9e0ac98610
calledit <1573053+calledit@users.noreply.github.com>
2024-08-28 02:07:30 +0200

    Get depth video

--- process_video.sh
+++ process_video.sh
@@ -7,13 +7,17 @@ mkdir data
 cd data
 rm -r imgs_4fps
 rm -r masks_4fps
+rm -r depth_4fps
 wget -O video.mp4 $3
 wget -O mask.mp4 $4
+wget -O depth.mp4 $5
 mkdir imgs_4fps
+mkdir depth_4fps
 mkdir masks_4fps
 
 ffmpeg -i video.mp4 -vf 'fps=4,scale=640:480' imgs_4fps/%06d.png
 ffmpeg -i mask.mp4 -vf 'fps=4,scale=640:480' masks_4fps/%06d.png
+ffmpeg -i depth.mp4 -vf 'fps=4,scale=640:480' depth_4fps/%06d.png
 
 cd ..
 scripts/demo.sh $1 $2
